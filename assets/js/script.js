110e6b5cde2ad79e39cdfa9c0f41d01deb5619c6
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-08 19:57:42 -0400

    found better endpoint and code is properly retrieving current day weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-function getWeatherData(searchTerm) {
+// get weather data including city name, forecast icon, temperature, humidity, and wind speed
+function getLocationData(searchTerm) {
     var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=d6c7088748687d06d3775770026215e2";
     fetch(currentApiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
-                    console.log("temperature: " + data.main.temp, String.fromCharCode(176) + "F, humidity: " + data.main.humidity + "%, wind speed: " + data.wind.speed + " MPH");
-                    getUvIndex(data.coord.lat, data.coord.lon);
+                    console.log(data.name, data.coord.lat, data.coord.lon);
+                    getWeatherData(data.name, data.coord.lat, data.coord.lon);
                 });
             } else {
                 console.log("(Error: " + response.statusText + ")");
@@ -17,22 +18,40 @@ function getWeatherData(searchTerm) {
         });
 }
 
-function getUvIndex(lat, lon) {
-    var uvApiUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=d6c7088748687d06d3775770026215e2";
-    fetch(uvApiUrl)
+// get date and UV Index data and change element's color depending on value
+function getWeatherData(city, lat, lon) {
+    var weatherDataApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=d6c7088748687d06d3775770026215e2";
+        fetch(weatherDataApiUrl)
         .then(function(response) {
             if (response.ok) {
-                response.json().then(function(data) {
-                    console.log(data);
-                    console.log("Date", data.date_iso, "UV Index", data.value);
-                })
+                response.json().then(function({current, daily}) {
+                    console.log(current);
+                    // current weather data
+                    console.log(
+                        "CURRENT" + "\n" + 
+                        "City Name: " + city  + "\n" + 
+                        "Forecast: " + current.weather[0].main + "\n" + 
+                        "Forecast Icon: " + current.weather[0].icon + "\n" +
+                        "Temperature: " + current.temp + String.fromCharCode(176) + "F" + "\n" + 
+                        "Humidity: " + current.humidity + "%" + "\n" + 
+                        "Wind Speed: " + current.wind_speed + " MPH"
+                    );
+                    // future weather data
+                    console.log("FUTURE")
+                });
             } else {
                 console.log("(Error: " + response.statusText + ")");
             }
         })
         .catch(function(error) {
-            console.log("(Unable to connect to OpenWeatherMap's UVI Endpoint)")
+            console.log("(Unable to connect to OpenWeatherMap's One Call Endpoint)")
         });
 }
 
-getWeatherData("atlanta");
+// render all the data from previous functions onto the page
+
+// when a city is searched, save in localStorage and add to search history
+
+// when page is loaded, turn city names in local storage into clickable elements that load data into the dashboard
+
+getLocationData("Atlanta");
