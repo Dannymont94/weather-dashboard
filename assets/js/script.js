b31d055b889ac5f81ca0871be03d5efa66622dbd
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-08 16:02:59 -0400

    properly fetching data from ultraviolet index endpoint

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ function getWeatherData(searchTerm) {
                 response.json().then(function(data) {
                     console.log(data);
                     console.log("temperature: " + data.main.temp, String.fromCharCode(176) + "F, humidity: " + data.main.humidity + "%, wind speed: " + data.wind.speed + " MPH");
+                    getUvIndex(data.coord.lat, data.coord.lon);
                 });
             } else {
                 console.log("(Error: " + response.statusText + ")");
@@ -13,6 +14,24 @@ function getWeatherData(searchTerm) {
         })
         .catch(function(error) {
             console.log("(Unable to connect to OpenWeatherMap's Current Weather Data Endpoint)");
+        });
+}
+
+function getUvIndex(lat, lon) {
+    var uvApiUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=d6c7088748687d06d3775770026215e2";
+    fetch(uvApiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    console.log("Date", data.date_iso, "UV Index", data.value);
+                })
+            } else {
+                console.log("(Error: " + response.statusText + ")");
+            }
+        })
+        .catch(function(error) {
+            console.log("(Unable to connect to OpenWeatherMap's UVI Endpoint)")
         });
 }
 
