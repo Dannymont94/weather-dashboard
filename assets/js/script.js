97594a26d5bafa5dba60f5dc103d3da0893c3c76
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-08 21:08:09 -0400

    reformatted date converting code into getFormattedDate function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// get weather data including city name, forecast icon, temperature, humidity, and wind speed
+// get location data for city searched by user
 function getLocationData(searchTerm) {
     var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=d6c7088748687d06d3775770026215e2";
     fetch(currentApiUrl)
@@ -18,32 +18,37 @@ function getLocationData(searchTerm) {
         });
 }
 
-// get date and UV Index data and change element's color depending on value
-function getWeatherData(city, lat, lon) {
+function getWeatherData(cityName, lat, lon) {
     var weatherDataApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=d6c7088748687d06d3775770026215e2";
         fetch(weatherDataApiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function({current, daily}) {
                     console.log(current);
-                    // get date
-                    var date = new Date(current.dt * 1000);
-                    var formattedDate = date.toLocaleString().split(",")[0];            
-                    // current weather data
+                    // get dates
+                    var currentDate = getFormattedDate(current.dt);
+                    // current weather data info needed: city name, date, forecast icon, temp, humidity, wind speed, and uv index
                     console.log(
                         "CURRENT" + "\n" + 
-                        "City Name: " + city  + "\n" + 
-                        "Date: " + formattedDate + "\n" + 
+                        "City Name: " + cityName + "\n" + 
+                        "Today's Date: " + currentDate + "\n" + 
                         "Forecast: " + current.weather[0].main + "\n" + 
                         "Forecast Icon: " + current.weather[0].icon + "\n" +
                         "Temperature: " + current.temp + String.fromCharCode(176) + "F" + "\n" + 
                         "Humidity: " + current.humidity + "%" + "\n" + 
-                        "Wind Speed: " + current.wind_speed + " MPH"
+                        "Wind Speed: " + current.wind_speed + " MPH" + "\n" + 
+                        "UV Index: " + current.uvi
                         );
+                    
                     console.log(daily);
-                    // future weather data starts at daily[1]
+                    // future weather data starts at daily[1]. info needed: date, forecast icon, temp, humidity
                     console.log(
-                        "FUTURE"
+                        "FUTURE" + "\n" + 
+                        "Tomorrow's Date: " + getFormattedDate(daily[1].dt) + "\n" + 
+                        "Forecast: " + "" + "\n" + 
+                        "Forecast Icon: " + "" + "\n" + 
+                        "Temperature: " + "" + "\n" + 
+                        "Humidity: " + "" + "\n"
                     );
                 });
             } else {
@@ -53,6 +58,10 @@ function getWeatherData(city, lat, lon) {
         .catch(function(error) {
             console.log("(Unable to connect to OpenWeatherMap's One Call API Endpoint)")
         });
+}
+
+function getFormattedDate(timestamp) {
+    return (new Date(timestamp * 1000)).toLocaleString().split(",")[0];
 }
 
 // render all the data from previous functions onto the page
