aa21c58e9248d65f99d3eb3607ee786cba10e080
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-09 14:18:05 -0400

    code now loads searchHistory from local storage and displays it to the page. clicking on a searchHistory div loads that city's weather data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,10 @@ function formSubmitHandler(event) {
     getLocationData(searchTerm);
 }
 
+function searchHistoryClickHandler(event) {
+    getLocationData(event.target.textContent);
+}
+
 // get location data for city searched by user
 function getLocationData(searchTerm) {
     var currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=d6c7088748687d06d3775770026215e2";
@@ -73,6 +77,7 @@ function renderWeatherData(cityName, current, daily) {
     currentUviEl.textContent = "UV Index: " + currentUvi;
 
     // future weather data starts at daily[1] and only need 5 days of info. info needed: date, forecast icon, temp, humidity
+    futureForecastContainerEl.innerHTML = "";
     for (let i = 1; i < 6; i++) {
         var futureForecastDivEl = document.createElement("div");
         futureForecastDivEl.classList.add("future-forecast");
@@ -120,15 +125,31 @@ function capitalizeWords(string) {
 function saveSearchHistory(cityName) {
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
     searchHistory.push(cityName);
+    searchHistory = searchHistory.filter(function(value, index, array) {
+        return array.indexOf(value) === index
+    });
     if (searchHistory.length > 10) {
         searchHistory = searchHistory.slice(1, 11);
     }
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    loadSearchHistory();
 }
 
 // when page is loaded, turn city names in local storage into clickable elements that load data into the dashboard
+function loadSearchHistory() {
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    searchHistoryListEl.innerHTML = "";
+
+    for (let i = 0; i < searchHistory.length; i++) {
+        var searchHistoryListItemEl = document.createElement("li");
+        searchHistoryListItemEl.textContent = searchHistory[i];
+
+        searchHistoryListEl.prepend(searchHistoryListItemEl);
+    }
+}
 
 
 
-
+loadSearchHistory();
 citySearchFormEl.addEventListener("submit", formSubmitHandler);
+searchHistoryListEl.addEventListener("click", searchHistoryClickHandler);
